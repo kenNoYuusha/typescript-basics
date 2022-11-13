59c3461b5a7f9d079c2a290ae927dd75465207b4
Ren Amaki <jorge_luis1709@hotmail.com>
2022-11-12 22:51:52 -0600

    tipos inferidos

--- src/03-typing.ts
+++ src/03-typing.ts
@@ -1,1 +1,15 @@
-/* Tipos inferidos */
+/* 
+    Tipos inferidos 
+    El motor de inferencia le asigna un tipo de dato al momento de inicializar la variable
+*/
+
+let myProductName = 'Product 1';
+let myProductPrice = 123;
+
+myProductName = "CHANGE";
+myProductName = myProductName.toLowerCase();
+console.log(myProductName);
+
+const myProductStock = 1000;
+
+const myProductName2 = "Product 1";
